7f960346044f210c257784d3b0f1fa700d8b5f5e
60138546 <1011888891@qq.com>
2024-09-13 10:39:03 +0800

    提交版本

--- CheckIdCard.js
+++ CheckIdCard.js
@@ -1,6 +1,6 @@
 import idcard from 'idcard-tool';// 假设 idcard-tool 提供了浏览器兼容的版本
 
-export function CheckIdCard(number) {
+export const CheckIdCard = async (number) => {
     const numberStr = typeof number === 'number' ? number.toString() : number;
     if (typeof numberStr !== 'string' || !/^(?:[0-9]{18}|[JZ][0-9]{16}[0-9Xx])$/i.test(numberStr)) {
         throw new Error('数据格式不正确，number 字段必须是有效的身份证号码字符串');
